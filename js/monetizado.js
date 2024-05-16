066072547be1851290411aeac4b14a9ddbe0b36c
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2024-05-16 11:48:20 -0400

    update with lisk

--- js/monetizado.js
+++ js/monetizado.js
@@ -8,6 +8,7 @@ const networks = {
 	"botanix:testnet" : "0xd0876600e82CCAa4aA0ab0Cd8bEa9c74F5b46De3",
 	"etherlink:testnet" : "0x4dA05ddA2F0586327E956548fd3E88a508ba2168",
 	"filecoin:testnet" : "0x6388418CfEa921C323b91B173E10D7f8f1193680",
+	"lisk:testnet" : "0x4dA05ddA2F0586327E956548fd3E88a508ba2168",
 	"mode:testnet" : "0xf0F90a236c860FB79DbBA4Ad9Dd62b714268BD21",
 	"monetichain:testnet" : "0xd0876600e82CCAa4aA0ab0Cd8bEa9c74F5b46De3",
 	"morph:testnet":"0x86f5304600627e7897AaAfAD39853e3D18E71B43",
